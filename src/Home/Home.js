401a33c001b2fbf33749b0e083a2e565585d2826
Anne Marinho <annecmarinho@yahoo.com.br>
2021-10-15 17:21:51 -0300

    edição do home

--- src/Home/Home.js
+++ src/Home/Home.js
@@ -1,7 +1,59 @@
-import React from "react";
+import {useState, React, render} from "react";
+import {Carousel} from "react-bootstrap";
+import "./Home.css"
 
 function Home(){
-    return <h1>Home</h1>
-}
+
+    const [index, setIndex] = useState(0);
+  
+    const handleSelect = (selectedIndex, e) => {
+      setIndex(selectedIndex);
+    };
+  
+    return (
+        <div className="Bloco 1">
+            <p className="título">Livros mais populares</p>
+        <div className="Carrossel">
+      <Carousel activeIndex={index} onSelect={handleSelect}>
+          
+        <Carousel.Item>
+          <img
+            className="d-block w-100"
+            src="holder.js/800x400?text=First slide&bg=373940"
+            alt="First slide"
+          />
+        </Carousel.Item>
+        <Carousel.Item>
+          <img
+            className="d-block w-100"
+            src="holder.js/800x400?text=Second slide&bg=282c34"
+            alt="Second slide"
+          />
+  
+          <Carousel.Caption>
+            <h3>Second slide label</h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+          </Carousel.Caption>
+        </Carousel.Item>
+        <Carousel.Item>
+          <img
+            className="d-block w-100"
+            src="holder.js/800x400?text=Third slide&bg=20232a"
+            alt="Third slide"
+          />
+  
+          <Carousel.Caption>
+            <h3>Third slide label</h3>
+            <p>
+              Praesent commodo cursus magna, vel scelerisque nisl consectetur.
+            </p>
+          </Carousel.Caption>
+        </Carousel.Item>
+    
+      </Carousel>
+       </div>
+    </div>
+    );
+    }
 
 export default Home;
