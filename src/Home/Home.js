0cf021b8d6b38e08f2fb5be62feb5a2d74df391f
Anne Marinho <annecmarinho@yahoo.com.br>
2021-10-20 13:17:52 -0300

    carrossel + sobre nos

--- src/Home/Home.js
+++ src/Home/Home.js
@@ -1,58 +1,68 @@
+import { display, positions } from "@material-ui/system";
 import {useState, React, render} from "react";
 import {Carousel} from "react-bootstrap";
 import "./Home.css"
 
 function Home(){
 
-    const [index, setIndex] = useState(0);
-  
-    const handleSelect = (selectedIndex, e) => {
-      setIndex(selectedIndex);
-    };
+
+      
   
     return (
-        <div className="Bloco 1">
-            <p className="título">Livros mais populares</p>
-        <div className="Carrossel">
-      <Carousel activeIndex={index} onSelect={handleSelect}>
+        <div className="PaginaHome">
+
+          <div className="Carrossel">
+          <h3 className="Title">Livros mais populares</h3>
           
-        <Carousel.Item>
+          
+          <Carousel style={{height:"300px",top:"100px",backgroundColor:"#C4C4C4",borderRadius:"19px",width:"900px"}} >
+
+           <Carousel.Item style={{display:"flex", flexDirection: "row",alignItems:"center"}}>
           <img
-            className="d-block w-100"
-            src="holder.js/800x400?text=First slide&bg=373940"
+            style={{margin:"16px 30px 16px 100px",width:"150px"}}
+            className="d-block "
+            src="/images/Sapiens 1.png"
             alt="First slide"
           />
-        </Carousel.Item>
-        <Carousel.Item>
-          <img
-            className="d-block w-100"
-            src="holder.js/800x400?text=Second slide&bg=282c34"
-            alt="Second slide"
-          />
+          <p style={{fontFamily:"Overblock SC",fontSize:"1.0em",textJustify:"inter-word"}}>Um dos maiores fenômenos editoriais dos últimos tempos
+    O que possibilitou ao Homo sapiens subjugar as demais espécies? O que nos torna capazes das mais belas obras de arte, dos avanços científicos mais impensáveis e das mais horripilantes guerras?
+    Nossa capacidade imaginativa. Somos a única espécie que acredita em coisas que não existem na natureza, como Estados, dinheiro e direitos humanos.</p>
   
-          <Carousel.Caption>
-            <h3>Second slide label</h3>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-          </Carousel.Caption>
-        </Carousel.Item>
-        <Carousel.Item>
-          <img
-            className="d-block w-100"
-            src="holder.js/800x400?text=Third slide&bg=20232a"
-            alt="Third slide"
-          />
-  
-          <Carousel.Caption>
-            <h3>Third slide label</h3>
-            <p>
-              Praesent commodo cursus magna, vel scelerisque nisl consectetur.
-            </p>
-          </Carousel.Caption>
-        </Carousel.Item>
-    
-      </Carousel>
-       </div>
+  </Carousel.Item>
+
+  <Carousel.Item style={{display:"flex", flexDirection: "row",alignItems:"center"}}>
+    <img
+    style={{margin:"16px 30px 16px 100px",width:"150px"}}
+      className="d-block"
+      src="/images/capa-o milagre da manhã.jpg"
+      alt="Second slide"
+    />
+
+   
+  </Carousel.Item>
+
+  <Carousel.Item style={{display:"flex", flexDirection: "row",alignItems:"center"}}>
+    <img
+      style={{margin:"16px 30px 16px 100px",width:"150px"}}
+      className="d-block"
+      src="/images/capa- a culpa é das estrelas.jpg"
+      alt="Third slide"
+    />
+
+   
+  </Carousel.Item>
+</Carousel>
+
+     
+      </div>
+      <div className="Sobre" >
+      <img 
+      style={{width: "1500px",
+        height: "500px"}}
+      src="/images/Frame 2.png"/>
+       <p className="SobreText">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum</p>
     </div>
+      </div>
     );
     }
 
