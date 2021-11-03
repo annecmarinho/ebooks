39920152bfd3df5c96efa970f0c6bdfdccc75f38
Anne Marinho <annecmarinho@yahoo.com.br>
2021-11-03 19:07:05 -0300

    setando user id no session storage

--- src/services/auth.js
+++ src/services/auth.js
@@ -1,10 +1,14 @@
 export const TOKEN_KEY ="@ebooks-Token"; 
+export const USER_KEY ="@ebooks-User";
 export const isAuthenticated =() => sessionStorage.getItem(TOKEN_KEY) !== null; 
 export const getToken = () => sessionStorage.getItem(TOKEN_KEY); 
-export const login = (token) => { 
+export const login = (token, user) => { 
     sessionStorage.setItem(TOKEN_KEY,token); 
+    sessionStorage.setItem(USER_KEY,user); 
 }; 
  
 export const logout = () =>{ 
     sessionStorage.removeItem(TOKEN_KEY); 
+    sessionStorage.removeItem(USER_KEY); 
 };
+export const userId = sessionStorage.getItem(USER_KEY);
